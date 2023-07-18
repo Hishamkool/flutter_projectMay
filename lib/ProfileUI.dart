48bc7b15d1e98325986cfcfd180bba5bb28daac0
Muhammed hisham <hishamkool@gmail.com>
2023-07-18 22:39:26 +0530

    home 18-07-2023
    1. ProfileUI.dart with settings at bottom . (need to add single child scroll view
    )

---
+++ lib/ProfileUI.dart
@@ -0,0 +1,179 @@
+import 'package:eva_icons_flutter/eva_icons_flutter.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: profileUI(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
+
+
+
+class profileUI extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      body: Container(
+        // child: SliverAppBar(
+        //   expandedHeight: 100, //change later
+        //   leading: IconButton(
+        //     onPressed: () {},
+        //     icon: Icon(Icons.arrow_left),
+        //   ),
+        //   actions: [IconButton(onPressed: () {}, icon: Icon(Icons.menu))],
+        // ),
+
+        child: ListView(
+          children: [
+            Row(
+              children: [
+                Expanded(
+                  child: Align(
+                    alignment: Alignment.topLeft,
+                    child: IconButton(
+                      onPressed: () {},
+                      icon: Icon(
+                        Icons.arrow_left,
+                        size: 35,
+                      ),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Align(
+                    alignment: Alignment.topRight,
+                    child: IconButton(
+                      onPressed: () {},
+                      icon: Icon(Icons.menu),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Center(
+              child: Container(
+                margin: EdgeInsets.all(20),
+                height: 200,
+                width: 200,
+                decoration: BoxDecoration(
+                  color: Colors.grey,
+                  image: DecorationImage(
+                      image: AssetImage('assets/images/cat heart.png')),
+                  borderRadius: BorderRadius.circular(100),
+                ),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(left: 100, right: 100, top: 25),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(EvaIcons.facebook),
+                  ),
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(EvaIcons.google),
+                  ),
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(EvaIcons.twitter),
+                  ),
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(
+                      EvaIcons.linkedin,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            //
+            SizedBox(height: 20),
+            Center(
+              child: ListTile(
+                title: Center(
+                    child: Text(
+                  'Hisham',
+                  style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                )),
+                subtitle: Center(
+                    child: Text(
+                  'Flutter developer',
+                  style: TextStyle(fontSize: 15),
+                )),
+              ),
+            ),
+
+            //
+            Container(
+              margin: EdgeInsets.all(15),
+              padding: EdgeInsets.all(40),
+              child: Center(
+                  child: Text(
+                'Mobile App Developer and Open Source Enthusiastic',
+                style: TextStyle(fontSize: 20),
+              )),
+            ),
+            Container(
+              // margin: EdgeInsets.only(left: 25, right: 25),
+              child: Column(
+                children: [
+                  scrollContainer(
+                    leadingIcon: Icons.privacy_tip,
+                    listTitle: 'Privacy',
+                  ),
+                  SizedBox(height: 10,),
+                  scrollContainer(
+                    leadingIcon: Icons.privacy_tip,
+                    listTitle: 'Privacy',
+                  ),
+                  SizedBox(height: 10,),
+                  scrollContainer(
+                    leadingIcon: Icons.privacy_tip,
+                    listTitle: 'Privacy',
+                  ),
+                  SizedBox(height: 10,),
+                  scrollContainer(
+                    leadingIcon: Icons.privacy_tip,
+                    listTitle: 'Privacy',
+                  ),
+                  SizedBox(height: 10,),
+                  scrollContainer(
+                    leadingIcon: Icons.privacy_tip,
+                    listTitle: 'Privacy',
+                  ),
+                  SizedBox(height: 10,),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+class scrollContainer extends StatelessWidget {
+  final IconData? leadingIcon;
+  final String listTitle;
+
+
+  scrollContainer({Key? key,  required this.leadingIcon, required this.listTitle}):super (key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(left: 50, right: 50),
+      decoration: BoxDecoration(color: Colors.grey[300],borderRadius: BorderRadius.circular(100)),
+      child: ListTile(
+        leading: Icon(leadingIcon) ,
+        title: Text(listTitle),
+        trailing: Icon(Icons.arrow_forward_ios),
+      ),
+    );
+  }
+}
