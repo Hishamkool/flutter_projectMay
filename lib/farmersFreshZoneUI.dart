80f6963c5c27f38c7162278196e41fe6dbed39c0
Muhammed hisham <hishamkool@gmail.com>
2023-07-21 14:31:16 +0530

    class 21-07-2023
    1. farmersFreshZoneUI.dart finished

--- lib/farmersFreshZoneUI.dart
+++ lib/farmersFreshZoneUI.dart
@@ -9,14 +9,30 @@ void main() {
   ));
 }
 
-class farmersFreshZone extends StatelessWidget {
+class farmersFreshZone extends StatefulWidget {
+  @override
+  State<farmersFreshZone> createState() => _farmersFreshZoneState();
+}
+
+class _farmersFreshZoneState extends State<farmersFreshZone> {
+  var currentSelected = 0;
+
   var gridImages = [
-    "https://images.news18.com/ibnlive/uploads/2021/08/tomato1-16299798893x2.jpg",
-    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcROvtRXHaAOMuDO_2oW95H17oDFf6zyfJ1fpA&usqp=CAU",
-    "https://images.news18.com/ibnlive/uploads/2021/08/tomato1-16299798893x2.jpg",
-    "https://nationaltoday.com/wp-content/uploads/2021/06/National-Herbs-and-Spices-Day-1-640x514.jpg",
-    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSGnnQcNCeHzbkq9lu8hm_yj4EC9tvk4_5_TA&usqp=CAU",
-    "https://images.news18.com/ibnlive/uploads/2021/08/tomato1-16299798893x2.jpg",
+    "assets/images/FarmersFreshZone/vegitables/beetroot.jpg",
+    "assets/images/FarmersFreshZone/Exotic/passionfruit.jpg",
+    "assets/images/FarmersFreshZone/Exotic/carambola.jpg",
+    "assets/images/FarmersFreshZone/vegitables/broccoli.jpg",
+    "assets/images/FarmersFreshZone/Exotic/papaya.jpg",
+    "assets/images/FarmersFreshZone/Exotic/lychee.jpg",
+  ];
+
+  var gridTitles = [
+    'vegitables',
+    'fruits',
+    'exotic',
+    'vegitables',
+    'fruits',
+    'exotic',
   ];
 
   @override
@@ -36,8 +52,8 @@ class farmersFreshZone extends StatelessWidget {
             actions: [
               IconButton(onPressed: () {}, icon: Icon(Icons.map)),
               Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text('Kochi'),
+                padding: const EdgeInsets.only(top: 21.0,right: 10),
+                child: Text('Kochi',style: TextStyle(fontSize: 14,fontWeight: FontWeight.w400),),
               ),
             ],
             bottom: AppBar(
@@ -62,120 +78,196 @@ class farmersFreshZone extends StatelessWidget {
             ),
           ),
           SliverList(
-              delegate: SliverChildListDelegate([
-            Container(
-              height: 80,
-              // color: Colors.purple,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Container(
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        border: Border.all(color: Colors.green)),
-                    padding: EdgeInsets.all(12),
-                    child: Text(
-                      'Vegitables',
-                      style: TextStyle(color: Colors.green),
-                    ),
-                  ),
-                  Container(
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        border: Border.all(color: Colors.green)),
-                    padding: EdgeInsets.all(12),
-                    child: Text(
-                      'Fruits',
-                      style: TextStyle(color: Colors.green),
-                    ),
-                  ),
-                  Container(
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        border: Border.all(color: Colors.green)),
-                    padding: EdgeInsets.all(12),
-                    child: Text(
-                      'Exoitic Fruits',
-                      style: TextStyle(color: Colors.green),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            //Corosal slider
-            CarouselSlider(
-              options: CarouselOptions(
-                // height: 400,
-
-                autoPlay: true,
-                aspectRatio: 2.0,
-                enlargeCenterPage: true,
-              ),
-              items: [
-                Container(
-                  decoration: BoxDecoration(
-                      image: DecorationImage(
-                          image: NetworkImage(
-                              "https://s3.india.com/travel/wp-content/uploads/2014/10/Karjat.jpg"))),
-                ),
-                Container(
-                  decoration: BoxDecoration(
-                      image: DecorationImage(
-                          image: AssetImage(
-                              "https://static.toiimg.com/thumb/msid-65656614,width-748,height-499,resizemode=4,imgsize-280972/.jpg"))),
-                ),
-                Container(
-                  decoration: BoxDecoration(
-                      image: DecorationImage(
-                          image: AssetImage(
-                              "https://asean.org/wp-content/uploads/2022/09/Philippines-scaled.jpg"))),
-                ),
-                Container(
-                  decoration: BoxDecoration(
-                      image: DecorationImage(
-                          image: AssetImage(
-                              "assets/images/hotelRooms/room1.jpg"))),
-                ),
-              ],
-            ),
-            Container(
-              margin: EdgeInsets.all(12.0),
-              padding: EdgeInsets.all(2.0),
-              decoration:
-                  BoxDecoration(border: Border.all(color: Colors.black)),
-              child: Padding(
-                padding: const EdgeInsets.all(20.0),
+            delegate: SliverChildListDelegate([
+              Container(
+                height: 80,
+                // color: Colors.purple,
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: [
-                    IconTitle(
-                      icone: Icons.alarm,
-                      title: '30 min policy',
+                    Container(
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          border: Border.all(color: Colors.green)),
+                      padding: EdgeInsets.all(12),
+                      child: Text(
+                        'Vegitables',
+                        style: TextStyle(color: Colors.green),
+                      ),
                     ),
-                    IconTitle(
-                      icone: Icons.camera_front_outlined,
-                      title: 'Traceblity',
+                    Container(
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          border: Border.all(color: Colors.green)),
+                      padding: EdgeInsets.all(12),
+                      child: Text(
+                        'Fruits',
+                        style: TextStyle(color: Colors.green),
+                      ),
                     ),
-                    IconTitle(
-                      icone: Icons.home_work,
-                      title: 'Local Sourrounding',
+                    Container(
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          border: Border.all(color: Colors.green)),
+                      padding: EdgeInsets.all(12),
+                      child: Text(
+                        'Exoitic Fruits',
+                        style: TextStyle(color: Colors.green),
+                      ),
                     ),
                   ],
                 ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(left: 10.0, top: 10),
-              child: Text(
-                'Shop By category:',
-                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
-              ),
-            ),
+              //Corosal slider
+              CarouselSlider(
+                options: CarouselOptions(
+                  // height: 400,
 
-          ]))
+                  autoPlay: true,
+                  aspectRatio: 2.0,
+                  enlargeCenterPage: true,
+                ),
+                items: [
+                  Container(
+                    decoration: BoxDecoration(
+                        image: DecorationImage(
+                            image: AssetImage(
+                                "assets/images/FarmersFreshZone/farms/farm1.jpg"))),
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                        image: DecorationImage(
+                            image: AssetImage(
+                                "assets/images/FarmersFreshZone/farms/farm2.jpg"))),
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                        image: DecorationImage(
+                            image: AssetImage(
+                                "assets/images/FarmersFreshZone/farms/farm3.jpg"))),
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                        image: DecorationImage(
+                            image: AssetImage(
+                                "assets/images/FarmersFreshZone/farms/farm4.jpg"))),
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                        image: DecorationImage(
+                            image: AssetImage(
+                                "assets/images/FarmersFreshZone/farms/farm5.jpg"))),
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                        image: DecorationImage(
+                            image: AssetImage(
+                                "assets/images/FarmersFreshZone/farms/farm7.jpg"))),
+                  ),
+                ],
+              ),
+              Container(
+                margin: EdgeInsets.all(12.0),
+                padding: EdgeInsets.all(2.0),
+                decoration:
+                    BoxDecoration(border: Border.all(color: Colors.black)),
+                child: Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      IconTitle(
+                        icone: Icons.alarm,
+                        title: '30 min policy',
+                      ),
+                      IconTitle(
+                        icone: Icons.camera_front_outlined,
+                        title: 'Traceblity',
+                      ),
+                      IconTitle(
+                        icone: Icons.home_work,
+                        title: 'Local Sourrounding',
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 10.0, top: 10),
+                child: Text(
+                  'Shop By category:',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                ),
+              ),
+              SizedBox(
+                height: 15,
+              ),
+            ]),
+          ),
+          SliverGrid(
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 3,
+              // childAspectRatio: 1.1,
+            ),
+            delegate: SliverChildBuilderDelegate(
+              (BuildContext context, int index) {
+                return Column(
+                  children: [
+                    Container(
+                      height: MediaQuery.of(context).size.height / 9,
+                      width: MediaQuery.of(context).size.width / 4,
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                            image: AssetImage(gridImages[index]),
+                            fit: BoxFit.cover),
+                        borderRadius: BorderRadius.circular(15.0),
+                        boxShadow: [
+                          BoxShadow(color: Colors.black, blurRadius: 20),
+                        ],
+                      ),
+                      // child: Text(gridTitles[index]),
+                      // child: Column(
+                      //
+                      //   children: [
+                      //     Image(image: NetworkImage(gridImages[index])),
+                      //     Text(gridTitles[index]),
+                      //   ],
+                      // ),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Text(gridTitles[index]),
+                  ],
+                );
+              },
+              childCount: gridImages.length,
+            ),
+          ),
         ],
       ),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: Colors.green[900],
+        selectedItemColor: Colors.white,
+        unselectedItemColor: Colors.green[100],
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.shopping_cart), label: 'Cart'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.account_circle), label: 'Account'),
+        ],
+        currentIndex: currentSelected,
+        onTap: onTapBottom,
+      ),
     );
+  }
+
+  void onTapBottom(int index) {
+    setState(() {
+      currentSelected = index;
+    });
   }
 }
 
